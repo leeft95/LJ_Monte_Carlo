645246c72f2b89d4f8c802e75b0fed67c0e71e77
leeft95 <leevan.trindade@gmail.com>
2016-10-11 16:51:09 +0100

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -14,23 +14,27 @@ def mag(a):
 def scalar(a, scalar):
     return a*scalar
 
-#The vector difference between two vectors
+#The vector difference between three vectors
 def sub(a, b,c):
-    c = a-b
-    return c
+    d = a-b-c
+    return d
 
 #The vector sum between two vectors
 def add(a, b,c):
-    c=a-b
-    return c
+    d = a-b
+    return d
 
 
-#Cross product of two vectors
+#Cross product of three vectors
 def cross(a,b,c):
-    c= np.cross(a,b)
-    return c
+    d = scalar(b,dot(a,c)) - scalar(c,dot(a,b))
+    return d
 
-#Dot product of two vectors
+#Dot product of three vectors
 def dot(a,b,c):
-    c=np.inner(a,b)
-    return c
+    if c == null
+        d = np.inner(a,b)
+        return d
+    else
+        d = np.inner(a,b,c)
+    return d
