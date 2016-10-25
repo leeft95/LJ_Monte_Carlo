0ee329b9625911b4d2b721d8ec9f9744ac997beb
Neon Barnacle <dafyddc@me.com>
2016-10-25 16:20:20 +0100

    Update VectorTest.py

--- VectorTest.py
+++ VectorTest.py
@@ -19,7 +19,7 @@ v2 = np.array([float(tokens[0]), float(tokens[1]), float(tokens[2])])
 line = inFile.readline()
 tokens = line.split()
 v3 = np.array([float(tokens[0]), float(tokens[1]), float(tokens[2])])
-# Print out all 3 numbers for every 3 do
+# Print all 3 numbers for all 3 vectors
 print "v1 = " + str(v1)
 print "v2 = " + str(v2)
 print "v3 = " + str(v3)
@@ -27,25 +27,22 @@ print ""
 
 # Test vector methods:
 # magnitude, summing , dot product, cross product 
-
-#print the magnitude of numpy array v1 to console
 print "magnitude of:\n v1 = " + str(vec.mag(v1))
-#print vector sum of numpy arrays v1, v2 and v3 to console 
 print "Vector sum of: \n v1, v2 and v3 = " + str(vec.add(vec.add(v1,v2),v3))
-#print dot product of numpy arrays v1, v2 and v3 to console 
 print "dot product of:\n v1.v2 = " + str(vec.dot(v1,v2))
-#print cross product of numpy arrays v1, v2 and v3 to console 
 print "cross product of: \n v1 x v2 =" + str(vec.cross(v1,v2))
 
 
 # Test vector operations:
 """The numpy vector operations are printed to the terminal using strings
   There are three "Show that..." statements below, which are demonstrated 
-  step by step and then printed to the terminal"""
+  step by step and then printed to the terminal
+"""
+
 
 """
 first "show that.." statement
-Print results of v1 x v2 and -v2 x v1 respectively to console to show that they are equal
+Demonstrating result  v1 x v2 = -v2 x v1 
 """  
 print " \nShow that the cross product of v1 x v2 = -v2 x v1"
 v4 = vec.scalar(v2,-1)
@@ -55,7 +52,7 @@ print " v1 x v2 = " + str(vec.cross(v1,v2))
 
 """
 Second "show that.." statement
-Print results of v1 x (v2 + v3) and (v1 x v2) +(v1 x v3)  respectively to console to show that they are equal
+Demonstrating result v1 x (v2 + v3) and (v1 x v2) + (v1 x v3) 
 """  
 print "\nShow v1 x (v2 + v3) = (v1 x v2) +(v1 x v3)"
 print "v1 x (v2 + v3) = " + str(vec.cross(v1,vec.add(v2,v3)))
@@ -68,9 +65,8 @@ print "(v1 x v2) +(v1 x v3) = " + str(v4)
 
 """
 Third "show that.." statement
-Print results of v1 x (v2 x v3) and (v1.v3)v2 - (v1.v2)v3  respectively to console to show that they are equal
+Demonstrating result v1 x (v2 x v3) and (v1.v3)v2 - (v1.v2)v3 
 """  
-#Third "show that..." statement
 print "\nShow v1 x (v2 x v3) = (v1.v3)v2 - (v1.v2)v3"
 print "v1 x (v2 x v3) = " + str(vec.cross(v1,vec.cross(v2,v3)))
 print "(v1.v3)v2 = " + str(vec.scalar(v2,vec.dot(v1,v3)))
