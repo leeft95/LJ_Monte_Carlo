11671b7bd0eff4ad2707fad5d83b083684c9ade9
Neon Barnacle <dafyddc@me.com>
2016-10-25 14:51:00 +0100

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -5,8 +5,8 @@ import numpy as np #import as np, shortened for easier use in this script
 #square magnitude
 def sqmag(a):
 """ Return the square magnitude of a vector
-    param a:  vector a
-    return: vector a dotted with itself, equivalent to the square of the magnitude of a"""
+    :param a:  3 dimensional numpy array a
+    :return: vector a dotted with itself, equivalent to the square of the magnitude of a"""
     return a*a
 
 # Magnitude of a vector
@@ -14,7 +14,7 @@ def sqmag(a):
 def mag(a):
     """
     return the magnitude of a vector
-    :param a: 3 dimesnionsal vector a
+    :param a: 3 dimensional numpy array a
     :return: magnitude of a
     """ 
     b = sqmag(a)
@@ -22,18 +22,20 @@ def mag(a):
 
 # Scalar Multiple of a vector
 def scalar(a, scalar):
-"""Multiplying a vector by a scalar
-    param a: 3D vector a
-    param scalar: any real integer or fraction that will be inputted by the user
-    return the product scalar*a  """
+    """
+    Multiplying a vector by a scalar
+    :param a: 3 dimensional numpy array a
+    :param scalar: any real integer or fraction that will be inputted by the user
+    :return: the product scalar*a
+    """
     return a*scalar
 
 #The vector difference between two vectors
 def sub(a, b):
     """
     subtract one vector from another 
-    :param a: 3 dimesnionsal vector a
-    :param b: 3 dimesnionsal vector b
+    :param a: 3 dimensional numpy array a
+    :param b: 3 dimensional numpy array b
     :return: vector diffreence between a and b 
     """ 
     d = a-b
@@ -41,10 +43,12 @@ def sub(a, b):
 
 #The vector sum between two vectors
 def add(a,b):
-"""" Vector sum
-    param a: first 3D vector a
-    param b: second 3D vector b
-    return the sum a+b labelled d"""
+    """" 
+    Vector sum
+    :param a: 3 dimensional numpy array a
+    :param b: 3 dimensional numpy array b
+    :return: the sum a+b labelled d
+    """
     d = a+b
     return d
 
@@ -52,8 +56,8 @@ def add(a,b):
 def cross(a,b)
     """
     return the cross product of two vectors
-    :param a: 3 dimesnionsal vector a
-    :param b: 3 dimesnionsal vector b
+    :param a: 3 dimensional numpy array a
+    :param b: 3 dimensional numpy array b
     :return: cross product axb
     """ 
     d = np.array[(a[2]*b[3] - a[3]*b[2]),(a[3]*b[1] - a[1]*b[3]),(a[1]*b[2] - a[2]*b[1])]
@@ -61,10 +65,12 @@ def cross(a,b)
 
 #Dot product of two vectors
 def dot(a,b):
-    """Scalar product
-    param a: first 3D vector a
-    param b: second 3D vector b 
-    return the sum of corresponding elements of a and b
-    (a[1]*b[1] + a[2]*b[2] + a[3]*b[3]) labelled d """
+    """
+    Scalar product
+    :param a: 3 dimensional numpy array a
+    :param b: 3 dimensional numpy array b
+    :return: the sum of corresponding elements of a and b
+    (a[1]*b[1] + a[2]*b[2] + a[3]*b[3]) labelled d
+    """
     d = a[1]*b[1] + a[2]*b[2] + a[3]*b[3]
     return d
